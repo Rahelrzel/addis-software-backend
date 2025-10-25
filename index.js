069b96845b95b9f6ebda45e8aaf76e64929a62ff
Rahelrzel <rahelrzele@gmail.com>
2025-10-25 03:00:50 -0700

    test route

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import testRoute from "./routes/test.js";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", testRoute);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
